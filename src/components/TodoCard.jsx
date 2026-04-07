b11bf7d5b16be10d1e23e573d9fab4d3c4f862a4
Nader PJR <pjrnader@gmail.com>
2026-04-07 18:31:33 +0430

    ui created

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const TodoCard = () => {
+const TodoCard = (props) => {
+
+    const { children } = props;
+
     return (
-        <div>TodoCard</div>
+        <li className='todoItem'>
+            {children}
+            <div className='actionsContainer'>
+                <i className="fa-solid fa-pen-to-square"></i>
+                <i className="fa-solid fa-trash"></i>
+            </div>
+
+        </li>
     )
 }
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 const TodoList = () => {
+
+    let todos = [
+        'Go to the gym',
+        'Go to the supermarket',
+        'Go to the park',
+    ]
+
     return (
-        <div>TodoList</div>
+        <ul className='main'>
+            {todos.map((todo, todoIndex) => {
+                return (
+                    <TodoCard key={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+
+                )
+            })}
+        </ul>
     )
 }
 
