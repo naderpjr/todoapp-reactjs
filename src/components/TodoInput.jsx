14d3adc909e55463fa2ec8f515eee7c277586dc4
Nader PJR <pjrnader@gmail.com>
2026-04-07 18:16:39 +0430

    first commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodoInput = () => {
+    return (
+        <header>
+            <input placeholder='Enter todo...' />
+            <button>Add</button>
+        </header>
+    )
+}
+
+export default TodoInput
